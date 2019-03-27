2e605928872a02ed901923d30a6a8d5e419df0b2
kidleee <1484906435@qq.com>
2019-03-27 21:36:55 +0800

    用于json数组有相同键值对拼接

---
+++ arr_merage.php
@@ -0,0 +1,29 @@
+<?php
+
+Class arr_merage extends Controller{
+    public function ArrayMerage(){
+      
+        //一维数组形式,圆括号中间没有[]就是一维数组
+        //当键值对相同使用array_merage,一维数组会覆盖重复已有键值对
+        $arr = array(
+            'name'=>$name,
+            'years'=>$years,
+            'cloth'=>$cloth,
+            'IdCard'=>$IdCard,
+        );
+        //想完成数组拼接必须是二维数组
+        //二维数组特点就是array([])中间必须有[]是代表二维
+        $arr = array([
+            'name'=>$name,
+            'years'=>$years,
+            'cloth'=>$cloth,
+            'IdCard'=>$IdCard,
+        ]);
+        //倘若转化为json数组
+        $json = json_encode($arr);
+        //若json数组转化为普通数组
+        $arr = json_decode($json,true);
+        //数组拼接一定是普通数组拼接。即使是json也要转变为二维普通数组拼接
+        $arr = $arr+$arr;
+    }
+}
